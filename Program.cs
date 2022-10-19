02ccea2c2fa4ab9f4c62dd9260f3577bec8455f8
saifmhr <saifmahar165@gmail.com>
2022-10-19 19:36:49 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace UserInputProgram
+{
+ class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Plaese enter letter : ");
+            int cValue = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine(cValue);
+
+
+            //convert to integer into string
+            Console.Write("Plaese enter first value : ");
+            int fValue=Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Plaese enter second value : ");
+            int sValue = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(fValue+sValue);
+
+            //simple string
+            Console.Write("Plaese enter your name : ");
+            string firstname = Console.ReadLine();
+
+            Console.Write("Plaese enter your name : ");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Helllo {0} , {1} ", firstname ,lastName);
+
+            Console.WriteLine("Hello " + firstname + ", " + lastName);
+        }
+    }
+}
